24714e0a0b603a3a9dadeb0b082aa17207c97ee0
esti28 <saarthak28@gmail.com>
2021-02-14 19:17:29 -0800

    Part1 complete

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,6 +3,7 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -14,6 +15,8 @@ public class ParseApplication extends Application {
 
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
 
+        ParseObject.registerSubclass(Post.class);
+
         // Use for monitoring Parse OkHttp traffic
         // Can be Level.BASIC, Level.HEADERS, or Level.BODY
         // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
@@ -23,8 +26,8 @@ public class ParseApplication extends Application {
         builder.networkInterceptors().add(httpLoggingInterceptor);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("AKpaPGSFrxC5KpyL8NDERDV3WfTZzcWWyFF7IBlT")
-                .clientKey("S0DCCdjHGC58ytqPufQieWbMaKhN9L7gWimYXH2z")
+                .applicationId("PxN7POi6Oe2BpOmKrabiiCv9H2ZeMaHXqvUob416")
+                .clientKey("NaBaqLWxPfYQXIHZTlOYUJzSFoZGVDiWVx5hNesa")
                 .server("https://parseapi.back4app.com")
                 .build()
         );
